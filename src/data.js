42de4b60cc38948ecececfa5e9e6e7ceb426a61f
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2021-06-09 17:01:41 +0600

    Data added

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    path: "./images/Autorepair.png",
+    desc: "Autorepair project",
+  },
+  {
+    id: 2,
+    path: "./images/Autorepair.png",
+    desc: "Cybershop project",
+  },
+  {
+    id: 3,
+    path: "./images/Instashop-preview.jpg",
+    desc: "Instashop project",
+  },
+  {
+    id: 4,
+    path: "./images/portfolio.JPG",
+    desc: "Portfolio project",
+  },
+  {
+    id: 5,
+    path: "./images/town-trips.jpg",
+    desc: "Town-Trips project",
+  },
+  {
+    id: 6,
+    path: "./images/soccer-sports.jpg",
+    desc: "Soccer-Sports project",
+  },
+];
+
+export default data;
